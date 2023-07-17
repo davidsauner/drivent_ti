28196fdd7b5035b6d341d71e0c297320b55ad249
DavidSauner <davidsauner@gmail.com>
2023-07-17 11:48:53 -0300

    Feat: Hotel routes

---
+++ src/routers/hotels-router.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { authenticateToken } from '../middlewares';
+import { getHotels, getHotelsWithRooms } from '@/controllers/hotel-controlller';
+
+const hotelsRouter = Router();
+
+hotelsRouter.all('/*', authenticateToken).get('/', getHotels).get(':/hotelId', getHotelsWithRooms);
+
+export { hotelsRouter };
