a7a86cfd9950f0a838dbdb5bce554d6411802d7f
DavidSauner <davidsauner@gmail.com>
2023-07-17 15:52:06 -0300

    Revert "Feat: Hotel routes"
    This reverts commit 28196fdd7b5035b6d341d71e0c297320b55ad249.

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,6 @@ import {
   ticketsRouter,
   paymentsRouter,
 } from '@/routers';
-import { hotelsRouter } from './routers/hotels-router';
 
 const app = express();
 app
@@ -29,7 +28,6 @@ app
   .use('/enrollments', enrollmentsRouter)
   .use('/tickets', ticketsRouter)
   .use('/payments', paymentsRouter)
-  .use('/hotels', hotelsRouter)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
